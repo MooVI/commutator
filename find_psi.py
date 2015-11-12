0e88a2f0159e17341b53b47fe12179f41a502016
Moo VI <metaknightdrake-git@yahoo.co.uk>
2015-11-12 18:51:56 +0000

    Add automation

---
+++ find_psi.py
@@ -0,0 +1,86 @@
+import commutator as comm
+from sympy import I, symbols, latex
+
+p = comm.print_group
+c = comm.calculate_commutator
+N = comm.Ncproduct
+
+
+V,J,f = symbols('V J f')
+L = 20
+orders = {V:1,f:1}
+p.orders = orders
+fpart = [N(I*f, [2*j+1,2*j+2]) for j in range(L)]
+Vpart = [N(V, [2*j+1,2*j+2,2*j+3,2*j+4]) for j in range(L-2)]
+small = fpart+Vpart
+Jpart = [N(I*J, [2*j+2,2*j+3]) for j in range(L-1)]
+H = fpart + Vpart +Jpart
+
+START_ORDER = 2
+END_ORDER = 4
+
+START_PSI = (N(1, 'a1')
+             + N(f/J, 'a2') + N(V/(I*J), 'b1 a2 a3'))
+START_IOFVARS = []
+
+FILEHEAD = 'testpsi'
+NORM_AS_YOU_GO = False
+
+
+psi = START_PSI
+iofvars = START_IOFVARS
+
+for test_order in range(START_ORDER, END_ORDER+1):
+    psi_sub = None
+    psi_test_sub = None
+    
+    Hcomm = c(H, psi)
+    if not comm.check_group_at_least_order(Hcomm, test_order-1, orders):
+        raise ValueError('Psi does not to order '+str(test_order-1)+'!')
+    subspace, matrixrows = comm.sparse_find_subspace(Hcomm, Jpart)
+    comm.print_subspace(subspace)
+    cvector = comm.build_vector_to_cancel(Hcomm, subspace)
+    subs_rules = {}
+    psi_test = comm.sparse_solve_for_commuting_term(cvector,
+                                                    psi,
+                                                    test_order,
+                                                    orders,
+                                                    matrixrows,
+                                                    subspace,
+                                                    norm = False,
+                                                    subs_rules = subs_rules,
+                                                    iofvars = iofvars,
+                                                    fvarname = 'F' + str(test_order) + '_')
+
+    for x in sorted(subs_rules.keys(), key = lambda x: int(str(x)[2+len(str(test_order)):])):
+        print(str(x)+': ' +str(subs_rules[x]))
+    print('\n')
+    psi = comm.substitute_group(psi, subs_rules)
+    
+    orders.update(zip(iofvars,[test_order]*len(iofvars)))
+    normdict = comm.check_normalisable(psi+psi_test, iofvars, test_order, orders)
+    for x in sorted(normdict.keys(), key = lambda x: int(str(x)[2+len(str(test_order)):])):
+        print(str(x)+': ' +str(normdict[x]))
+    psi_test_sub = comm.substitute_group(psi_test, normdict)
+    comm.save_group(psi_test_sub,
+                    FILEHEAD + '_' + str(test_order))
+    psi_sub = psi + psi_test_sub
+    comm.save_group(psi_sub, FILEHEAD)
+
+    psi += psi_test
+
+    if NORM_AS_YOU_GO:
+        prop = comm.square_to_find_identity(psi_sub)[0].scalar
+        with open(FILEHEAD+'_norm', mode = 'w') as f:
+            f.write(str(prop)+'\n\n')
+            f.write(latex(prop).replace('\\\\', '\\'))
+
+    
+
+if not NORM_AS_YOU_GO:
+    prop = comm.square_to_find_identity(psi_sub)[0].scalar
+    with open(FILEHEAD+'_norm', mode = 'w') as f:
+        f.write(str(prop)+'\n\n')
+        f.write(latex(prop).replace('\\\\', '\\'))
+        
+print('Done!')
