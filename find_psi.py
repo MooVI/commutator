dd57541b20d4be12288e341259a636b84bb223e0
Jack Kemp <jack.kemp@physics.ox.ac.uk>
2015-11-18 04:05:48 +0000

    Random psi changes

--- find_psi.py
+++ find_psi.py
@@ -16,20 +16,20 @@ small = fpart+Vpart
 Jpart = [N(I*J, [2*j+2,2*j+3]) for j in range(L-1)]
 H = fpart + Vpart +Jpart
 
-START_ORDER = 2
+START_ORDER = 7
 END_ORDER = 8
 
-START_PSI = (N(1, 'a1')
-             + N(f/J, 'a2') + N(V/(I*J), 'b1 a2 a3'))
+#START_PSI = (N(1, 'a1')
+#             + N(f/J, 'a2') + N(V/(I*J), 'b1 a2 a3'))
 
 START_IOFVARS = []
-START_SPLIT_ORDERS = [0, 1, 3]
+START_SPLIT_ORDERS = []
 
-#START_PSI = comm.load_group('testpsi_r4', START_IOFVARS, START_SPLIT_ORDERS)
+START_PSI = comm.load_group('speedpsi3_r6', START_IOFVARS, START_SPLIT_ORDERS)
 
 orders.update(zip(START_IOFVARS,[START_ORDER-1]*len(START_IOFVARS)))
 
-FILEHEAD = 'speedpsi3'
+FILEHEAD = 'speedpsiprof'
 NORM_AS_YOU_GO = True
 
 
