75236c1ce4f90fd612ea3064a05459085ccf3802
Jack Kemp <jack.kemp@physics.ox.ac.uk>
2015-11-24 13:05:44 +0000

    Change find_psi_dual

--- find_psi_dual.py
+++ find_psi_dual.py
@@ -17,15 +17,17 @@ small = fpart+V1part+V2part
 Jpart = [Ncproduct(I*J, [2*j+2,2*j+3]) for j in range(L-1)]
 H = fpart + V1part + V2part +Jpart
 
-START_ORDER = 2
+START_ORDER = 6
 END_ORDER = 6
 
 START_PSI = (N(1, 'a1')
              + N(f/J, 'a2') + N(V1/(I*J), 'b1 a2 a3'))
 START_IOFVARS = []
+START_SPLIT_ORDERS = [0, 1, 3]
 
-#START_PSI = comm.load_group('testpsi_r4', START_IOFVARS)
-#orders.update(zip(START_IOFVARS,[START_ORDER-1]*len(START_IOFVARS)))
+START_PSI = comm.load_group('psi_dual_r5', START_IOFVARS, START_SPLIT_ORDERS)
+
+orders.update(zip(START_IOFVARS,[START_ORDER-1]*len(START_IOFVARS)))
 
 FILEHEAD = 'psidual'
 NORM_AS_YOU_GO = True
@@ -33,6 +35,7 @@ NORM_AS_YOU_GO = True
 
 psi = START_PSI
 iofvars = START_IOFVARS
+split_orders = START_SPLIT_ORDERS
 
 for test_order in range(START_ORDER, END_ORDER+1):
     psi_sub = None
@@ -54,15 +57,16 @@ for test_order in range(START_ORDER, END_ORDER+1):
                                                     norm = False,
                                                     subs_rules = subs_rules,
                                                     iofvars = iofvars,
+                                                    split_orders = split_orders,
                                                     fvarname = 'F' + str(test_order) + '_')
 
     for x in sorted(subs_rules.keys(), key = lambda x: int(str(x)[2+len(str(test_order)):])):
         print(str(x)+': ' +str(subs_rules[x]))
     print('\n')
-    psi = comm.substitute_group(psi, subs_rules)
+    psi = comm.substitute_group(psi, subs_rules, split_orders)
 
     orders.update(zip(iofvars,[test_order]*len(iofvars)))
-    normdict = comm.check_normalisable(psi+psi_test, iofvars, test_order, orders)
+    normdict = comm.check_normalisable(psi+psi_test, iofvars, test_order, orders, split_orders)
     for x in sorted(normdict.keys(), key = lambda x: int(str(x)[2+len(str(test_order)):])):
         print(str(x)+': ' +str(normdict[x]))
     psi_test_sub = comm.substitute_group(psi_test, normdict)
@@ -71,7 +75,7 @@ for test_order in range(START_ORDER, END_ORDER+1):
 
     psi += psi_test
     comm.save_group(psi,
-                    FILEHEAD + '_r' + str(test_order), iofvars=iofvars)
+                    FILEHEAD + '_r' + str(test_order), iofvars=iofvars, split_orders=split_orders)
 
     if NORM_AS_YOU_GO:
         prop = comm.square_to_find_identity(psi_sub)[0].scalar
